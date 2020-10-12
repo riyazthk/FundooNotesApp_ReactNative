5060e6f95c47a4a04816332b9d2c18b2068b3d3b
riyazthk <er.riyaz2507@gmail.com>
2020-10-12 16:06:55 +0530

    [Riyaz]ADDED:To write a code to design a login page

--- src/pages/signInPage/LoginPage.js
+++ src/pages/signInPage/LoginPage.js
@@ -1,26 +1,24 @@
-import {View,Text} from 'react-native'
-import React,{Component} from 'react'
-import { Card, Input } from 'react-native-elements'
-class LoginPage extends Component{
-    render(){
-        return(
-            <View>
-                <View>
-                    <Card>
-                      <Input
-                      
-                      placeholder="enter a email"
-                      >
-                      </Input>
-                      <Input
-                      
-                      placeholder="enter a password"
-                      >
-                      </Input>
-                    </Card>
-                </View>
-            </View>
-        )
-    }
+import {Button, View} from 'react-native';
+import React, {Component} from 'react';
+import {Card, Input} from 'react-native-elements';
+import styles from './LoginPageStyle';
+class LoginPage extends Component {
+  render() {
+    return (
+      <View style={styles.headerScreen}>
+        <Card containerStyle={styles.outerCard}>
+          <View style={styles.innerCardEmail}>
+            <Input placeholder="enter a email" />
+          </View>
+          <View style={styles.innerCardPassword}>
+            <Input placeholder="enter a password" />
+          </View>
+          <View style={styles.innerCardSubmit}>
+            <Button title="submit" type="outline" />
+          </View>
+        </Card>
+      </View>
+    );
+  }
 }
-export default LoginPage
+export default LoginPage;
---
+++ src/pages/signInPage/LoginPageStyle.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  headerScreen: {
+    height: '100%',
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+  },
+  outerCard: {
+    height: '50%',
+  },
+  innerCardEmail: {
+    justifyContent: 'space-around',
+    height: '38%',
+  },
+  innerCardPassword: {
+    justifyContent: 'space-around',
+    height: '38%',
+  },
+  innerCardSubmit: {
+    justifyContent: 'space-around',
+    height: '20%',
+  },
+});
+export default styles;
